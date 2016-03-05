e19f92583cec3fa24116d7fb5209b6240c9c1a10
Stefan Schult <stefan.schult@finanzcheck.de>
2016-03-05 12:25:35 +0100

    step four

--- src/components/views/Messenger.js
+++ src/components/views/Messenger.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import TeamLogo from '../elements/TeamLogo'
 import Profile from '../elements/Profile'
 import ListChannelsBtn from '../elements/ListChannelsBtn'
+import ChannelList from '../containers/ChannelList'
 import JoinedChannelList from '../elements/JoinedChannelList'
 import IntroText from '../elements/IntroText'
 import styles from './Messenger.scss'
@@ -13,18 +14,29 @@ import styles from './Messenger.scss'
   })
 )
 export default class Messenger extends Component {
+  state = {
+    toggleChannelList: false
+  };
+
+  toggleChannelList() {
+    const toggleChannelList = !this.state.toggleChannelList
+    this.setState({toggleChannelList})
+  }
 
   render() {
+    const { toggleChannelList } = this.state
+    const channelList = toggleChannelList ? <ChannelList /> : null
 
     return (
       <div className={styles.container}>
         <div className={styles.nav}>
           <TeamLogo />
           <Profile />
-          <ListChannelsBtn />
+          <ListChannelsBtn onClick={this.toggleChannelList.bind(this)}/>
           <JoinedChannelList />
         </div>
         <div className={styles.chat}>
+          {channelList}
           <IntroText />
         </div>
       </div>
